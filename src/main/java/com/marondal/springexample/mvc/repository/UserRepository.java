55962535dd9f67515d0fad84a4bf1908fd9e8066
dulumary <lecture@hagulu.com>
2025-11-13 19:35:38 +0900

    [김인규]  - mvc 사이클 예제

---
+++ src/main/java/com/marondal/springexample/mvc/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.marondal.springexample.mvc.repository;
+
+import com.marondal.springexample.mvc.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserRepository {
+
+    // new_user 테이블의 가장 최근 추가된 행 조회
+    public User selectLastUser();
+
+    public int insertUser(
+            @Param("name") String name
+            , @Param("birthday") String birthday
+            , @Param("email") String email
+            , @Param("introduce") String introduce);
+}
